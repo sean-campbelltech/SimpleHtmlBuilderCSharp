766b6701ee4b7ea782406014780ab8f152162a83
sean <sean@campbelltech.co.za>
2023-06-07 22:33:20 +0200

    Fixed typo

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 HtmlPage htmlPage = new HtmlBuilder()
                 .AddTitle("Builder Pattern")
                 .AddHeader1("1. Definition:")
-                .AddParagraph("The builder pattern separate the construction of a complex object from its representation...")
+                .AddParagraph("The builder pattern separates the construction of a complex object from its representation...")
                 .AddHeader1("2. Class Diagram:")
                 .AddHeader2("2.1 Conceptual Diagram")
                 .AddParagraph("TODO: Add Conceptual Class Diagram here...")
